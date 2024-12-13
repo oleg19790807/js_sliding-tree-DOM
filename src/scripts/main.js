28ab8286d415c5c70c25df4b2810ee45cc275463
Oleg <scor1@ukr.net>
2024-12-13 12:09:09 +0100

    solve

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const treeHeaders = document.querySelectorAll('.tree > li > :first-child');
+
+// Wrap each header text in a span tag and add a click event
+treeHeaders.forEach((header) => {
+  // Wrap text content in a span tag
+  const span = document.createElement('span');
+
+  span.textContent = header.textContent;
+  header.textContent = '';
+  header.appendChild(span);
+
+  // Add click event to toggle visibility of child elements
+  span.addEventListener('click', () => {
+    const parentLi = header.parentElement;
+    const childrenUl = parentLi.querySelector('ul');
+
+    if (childrenUl) {
+      childrenUl.style.display =
+        childrenUl.style.display === 'none' ? '' : 'none';
+    }
+  });
+});
